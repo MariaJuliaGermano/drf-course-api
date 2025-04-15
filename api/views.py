1584048411fcb469a7f1dd1fa30bc40af03c6228
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2025-04-15 10:28:26 -0300

    feat: novas urls e serializers

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET']) #### Pra que usar um decorador aqui, qual finalidade e o que ele faz especificamente aqui
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
